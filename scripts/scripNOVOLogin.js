f96eac2a14f9a268104e9ceafb0e89935aa73172
LimaBernardo212 <mariahelenalima911@gmail.com>
2025-01-12 11:56:39 -0300

    12.01.25

--- scripts/scripNOVOLogin.js
+++ scripts/scripNOVOLogin.js
@@ -1,22 +1,17 @@
-document.querySelector("#registerForm").addEventListener("submit", (event) => {
-    event.preventDefault(); // Prevenir comportamento padrão do formulário
-    const pin = document.getElementById("pinc").value; // string
-    const name = document.getElementById("username").value; // string
-    const email = document.getElementById("email").value; // string
+const eyere = document.querySelector("#eyere");
+const pinc = document.querySelector("#pinc");
+const evereas = document.querySelector("#eyereas");
+const sobpinc = document.querySelector("#sobpinc");
+var everImageTrocar = "images/olho.png";
+var everImage = "images/olho (1).png";
+var everImageTrocar1 = "images/olho.png";
+var everImage1 = "images/olho (1).png";
+eyere.addEventListener("click", function (){
+    pinc.type = pinc.type === "password" ? "text" : "password";
+    document.getElementById("Everelson").src = everImage;
+    let aux = everImageTrocar
+    everImageTrocar = everImage
+    everImage = aux
+})
 
- 
-    const mongodb = require('mongodb').MongoClient;
-    const url = 'mongodb+srv://bernardolimarodrigues4:qXNquEiMlFAirlGS@cluster0.oh6r1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // string
-    mongodb.connect(url, (erro, banco) => {
-        if (erro) throw erro;
-        const dbo = banco.db("Usuario");
-        const obj = { senha: "A senha é " + pin, nome: "O nome é " + name, email: "O email é " + email }; // objeto
-        const col = "UserData"; // string
-        dbo.collection(col).insertOne(obj, (erro, resultado) => {
-            if (erro) throw erro;
-            console.log("Inserido com sucesso");
-            banco.close();
-        });
-    });
-});
 
--- scripts/scriptProgrammerLogin.js
+++ scripts/scriptProgrammerLogin.js
@@ -6,25 +6,6 @@ var everImageTrocar = "images/olho.png";
 var everImage = "images/olho (1).png";
 var everImageTrocar1 = "images/olho.png";
 var everImage1 = "images/olho (1).png";
-document.getElementById('loginc').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const username = document.getElementById('name').value;
-    const password = document.getElementById('pinc').value;
-    const errorMessage = document.getElementById('Error');
-    // Usuário e senha de exemplo
-    const validUsername = 'Bernardo Lima';
-    const validPassword = '12345678';
-
-    if (username === validUsername && password === validPassword) {
-        window.alert('Seja bem-vindo, ' + username + '!')
-        window.location.href = 'projects.html';
-       
-        // Redirecionar para outra página ou realizar outra ação
-    } else {
-        errorMessage.textContent = 'Usuário ou senha incorretos.';
-    }
-});
 eyere.addEventListener("click", function (){
     pinc.type = pinc.type === "password" ? "text" : "password";
     document.getElementById("Everelson").src = everImage;
